a0f9e48a24dbc446c3371d19e05bbda12c20b02d
Emanuel Victor Fontoura Cantalejo <emanuelfontoura03@gmail.com>
2022-11-27 17:02:56 -0300

    header finished

---
+++ js/script.js
@@ -0,0 +1,27 @@
+var menu_responsivo = window.document.querySelector('.menu-responsivo')
+
+function abrir_menu(){
+    if (menu_responsivo.style.display == "none"){
+        menu_responsivo.style.display = "flex"
+    }else{
+        menu_responsivo.style.display = "none"
+    }
+}
+
+function fechar_menu(){
+    menu_responsivo.style.display = "none"
+}
+
+document.addEventListener('mouseup', function(e){
+    if (!menu_responsivo.contains(e.target)){
+        menu_responsivo.style.display="none"
+    }
+})
+
+function menu(){
+    if (screen.width <= 1100){
+        menu_responsivo.style.display = "none"
+    }else if(screen.width  > 1100){
+        menu_responsivo.style.display = "none"
+    }
+}
